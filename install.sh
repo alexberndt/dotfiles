a8ae81b58a44e25b20aca840d7b4781d2568ce55
Alex Berndt <alexander@overstory.ai>
2022-02-10 18:23:26 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,71 @@
 #!/bin/sh
 
+echo "Running install script ..."
+
+# Install Core Dependencies
+sudo apt-get install -y build-essential git 
+
+# Install VS Code
+echo "Installing VS Code ..."
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
+
+echo "Installing Inkscape ..."
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt update
+sudo apt install inkscape
+
+echo "Installing GIMP ..."
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt-get update
+sudo apt-get install gimp
+
+echo "Install VLC player ..."
+sudo apt install -y vlc
+
+echo "Installing Tex Live ..."
+sudo apt-get install -y texlive-full
+
+echo "Installing Spotify Client ..."
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+echo "Installing Unity Tweak Tool ..."
+sudo add-apt-repository universe
+sudo apt install -y gnome-tweak-tool
+mkdir ~/.themes
+git clone https://github.com/vinceliuice/Mojave-gtk-theme.git ~/.themes/mojave-gtk-theme
+chmod +x ~/.themes/mojave-gtk-theme/install.sh
+
+sudo apt-get install -y libglib2.0-dev-bin
+
+~/.themes/mojave-gtk-theme/install.sh
+
+echo "Setting background ..."
+mkdir ~/Pictures/wallpapers
+wget https://raw.githubusercontent.com/alexberndt/dotfiles/main/wallpapers/google-earth-1.jpg -O /home/alex/Pictures/wallpapers/earth-1.jpg
+gsettings set org.gnome.desktop.background picture-uri file:////home/alex/Pictures/wallpapers/earth-1.jpg
+gsettings set org.gnome.desktop.screensaver picture-uri file:////home/alex/Pictures/wallpapers/earth-1.jpg
+
+
+sudo apt-get install terminator
+
+# Pyenv
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+
+sudo apt install -y zsh curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
 
 # wget https://raw.githubusercontent.com/alexberndt/dotfiles/main/.tmux.conf
